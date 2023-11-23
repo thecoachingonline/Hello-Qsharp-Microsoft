c41931539d1100ead4881e5c321056ff3325f345
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-23 00:48:38 +0000

    C# & Q#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_Quantum_Microsoft_QSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Program.qs
+++ Program.qs
@@ -1,3 +1,5 @@
+using System;
+
 namespace HelloWorld {
     open Microsoft.Quantum.Intrinsic;
 
